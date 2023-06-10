628f913d1ad51b17292a9fad58d87130cbf812a3
hakanacardev <80627473+mcavlak@users.noreply.github.com>
2023-06-10 11:33:24 +0300

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { ThemeProvider } from "@mui/material";
 import React from "react";
+import "./styles/reset.css";
 import { themeOptions } from "./styles/mui_theme";
+import MainTemplate from "./components/templates/MainTemplate";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import { useRoutes } from "react-router-dom";
 
 function App() {
-  return <ThemeProvider theme={themeOptions}></ThemeProvider>;
+  const element = useRoutes([
+    { path: "/", element: <Products /> },
+    { path: "/product/:productId", element: <ProductDetail /> },
+    { path: "*", element: <div>Bulunamadı!</div> },
+  ]);
+  return (
+    <ThemeProvider theme={themeOptions}>
+      <MainTemplate>{element}</MainTemplate>
+    </ThemeProvider>
+  );
 }
 
 export default App;
